50e04197a22d13d3595a14f3d8fd1582b019f944
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-09 22:36:10 -0700

    Tweaked the initial state for filteredProducts

--- src/store/products.js
+++ src/store/products.js
@@ -1,35 +1,38 @@
+
+const allProducts = [
+  {
+    key: 'macbook',
+    displayName: 'Macbook',
+    description: 'An Apple computer',
+    price: 999.99,
+    category: 'electronics',
+  },
+  {
+    key: 'iphone',
+    displayName: 'iPhone',
+    description: "The most addictive thing you'll ever own",
+    price: 1199.99,
+    category: 'electronics',
+  },
+  {
+    key: 'banana',
+    displayName: 'Banana',
+    description: 'A yellow fruit',
+    price: 0.99,
+    category: 'food',
+  },
+  {
+    key: 'apple',
+    displayName: 'Apple',
+    description: 'The fruit kind',
+    price: 2.49,
+    category: 'food',
+  },
+];
+
 const initialState = {
-  allProducts: [
-    {
-      key: 'macbook',
-      displayName: 'Macbook',
-      description: 'An Apple computer',
-      price: 999.99,
-      category: 'electronics',
-    },
-    {
-      key: 'iphone',
-      displayName: 'iPhone',
-      description: "The most addictive thing you'll ever own",
-      price: 1199.99,
-      category: 'electronics',
-    },
-    {
-      key: 'banana',
-      displayName: 'Banana',
-      description: 'A yellow fruit',
-      price: 0.99,
-      category: 'food',
-    },
-    {
-      key: 'apple',
-      displayName: 'Apple',
-      description: 'The fruit kind',
-      price: 2.49,
-      category: 'food',
-    },
-  ],
-  filteredProducts: [],
+  allProducts, 
+  filteredProducts: [...allProducts],
 };
 
 export default function productsReducer(state = initialState, { type, payload }) {
@@ -57,7 +60,7 @@ export const filterProducts = (category) => {
   };
 };
 
-export const resetActiveCategory = () => {
+export const resetProducts = () => {
   return {
     type: 'RESET',
   };
