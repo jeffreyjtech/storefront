6ff04e00c5a491028defa7623377cd2184c80006
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-09 22:35:47 -0700

    Completed product list component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,33 @@
-import Product from './Product';
+import { connect } from 'react-redux';
+import { filterProducts, resetProducts } from '../store/products';
 
-function Products() {
+import shortUUID from 'short-uuid';
+
+import Product from './Product';
+import { Grid } from '@mui/material';
+
+function Products({ filteredProducts, filterProducts, resetProducts }) {
   return (
     <>
-      <p>Products List WIP</p>
-      <Product />
+      <p>Browse our wares</p>
+      <Grid container spacing={2}>
+        {filteredProducts.map((product) => (
+          <Grid item xs={12} sm={6} md={3} lg={2}>
+            <Product key={shortUUID.generate()} product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
 
-export default Products;
+const mapStateToProps = ({ products }) => {
+  return { filteredProducts: products.filteredProducts };
+};
+
+const mapDispatchToProps = {
+  filterProducts,
+  resetProducts,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
