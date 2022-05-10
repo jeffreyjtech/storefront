878dae207a53e9cbe1d0f18ec2718e35ab1b8d42
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-09 21:26:47 -0700

    Created Categories store & reducer

---
+++ src/store/categories.js
@@ -0,0 +1,31 @@
+const initialState = {
+  activeCategory: 'electronics',
+  categories: [
+    { 
+      key: 'food', 
+      displayName: 'Food',
+      description: 'Human fuel' 
+    },
+    {
+      key: 'electronics',
+      displayName: 'Electronics',
+      description: 'Nerd fuel',
+    },
+  ],
+};
+
+export default function categoriesReducer(state = initialState, { type, payload }) {
+  const { categories } = state;
+
+  switch (type) {
+    case 'ACTIVE':
+      // action requires a payload object with property category
+      if (categories.includes(payload.category)) {
+        return { ...state, activeCategory: payload.category };
+      }
+      return state;
+
+    default:
+      return state;
+  }
+}
