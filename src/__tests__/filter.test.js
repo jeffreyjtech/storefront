2f51423dc5b9a7b6110503bebdbe89fbed14a44a
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-10 21:37:29 -0700

    Completed filter integration test

---
+++ src/__tests__/filter.test.js
@@ -0,0 +1,28 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+import { Provider } from 'react-redux';
+import createStore from '../store';
+
+test('Testing that product list can be filtered', () => {
+  render(
+    <Provider store={createStore()}>
+      <App />
+    </Provider>
+  );
+
+  const allProducts = screen.getAllByTestId(/product/i)
+
+  const selectElem = screen.getByTestId(/categories-select/i);
+  expect(selectElem).toHaveTextContent(/all/i)
+
+  const selectButtonElem = screen.getAllByRole('button')[0];
+  fireEvent.mouseDown(selectButtonElem );
+
+  const electronicsElem = screen.getByTestId(/category-electronics/i);
+  fireEvent.click(electronicsElem);
+
+  const filterProducts = screen.getAllByTestId(/product/i)
+
+  expect(filterProducts.length).toBeLessThan(allProducts.length);
+});
