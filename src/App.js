9df2d3f43bed790361e49adab86c1359677194c2
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-10 23:28:38 -0700

    Added SimpleCart component to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Header from './components/Header';
-import Categories from './components/Categories';
 import Products from './components/Products';
 import Footer from './components/Footer';
 import { Box } from '@mui/material';
+import SimpleCart from './components/SimpleCart';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Box sx={{ p: 2 }}>
         <Header />
         <Box sx={{ float: 'right' }}>
-          <Categories />
+          <SimpleCart />
         </Box>
         <Products />
         <Footer />
