55bf92af9ab728eddadeea5158af494d6161eff5
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-11 00:36:12 -0700

    Filled in SimpleCart component

--- src/components/SimpleCart.js
+++ src/components/SimpleCart.js
@@ -1,2 +1,20 @@
-import { useSelector, useDispatch } from "react-redux";
+import { List, ListItem, ListItemText } from '@mui/material';
+import { useSelector } from 'react-redux';
+import shortUUID from 'short-uuid';
 
+export default function SimpleCart() {
+  const contents = useSelector((state) => state.cart.contents);
+
+  return (
+    <List sx={{position: 'absolute', right:'100px'}}>
+      Your cart:
+      {contents.map((product) => (
+        <ListItem key={shortUUID.generate()}>
+          <ListItemText>
+            {product.displayName} <br/> qty: {product.quantity}
+          </ListItemText>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
