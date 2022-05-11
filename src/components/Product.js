448b889ee31beae960358b5bc1695ea0bf158bd4
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-10 20:31:19 -0700

    Added testid for testing

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,10 @@
 import { Card, CardContent, CardHeader } from '@mui/material';
 
 function Product({ product }) {
-  const { displayName, price, description } = product;
+  const { displayName, price, description, category, productId } = product;
 
   return (
-    <Card variant="outlined">
+    <Card variant="outlined" data-testid={`product-${category}-${productId}`}>
       <CardHeader title={displayName} />
       <CardContent>
         <p>{description}</p>
