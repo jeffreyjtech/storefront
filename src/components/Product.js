0bb3ed48c5ec5b4f5b9847725fcc17786af67541
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-10 21:08:29 -0700

    Added stock level to product cards

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,16 @@
-import { Card, CardContent, CardHeader } from '@mui/material';
+import { Button, Card, CardContent, CardHeader } from '@mui/material';
 
 function Product({ product }) {
-  const { displayName, price, description, category, productId } = product;
+  const { displayName, price, description, category, productId, stock } = product;
 
   return (
     <Card variant="outlined" data-testid={`product-${category}-${productId}`}>
       <CardHeader title={displayName} />
       <CardContent>
         <p>{description}</p>
-        <p>{price}</p>
+        <span>{price}</span> <span style={{ float: 'right' }}>In-stock: {stock}</span>
+        <hr />
+        <Button>Add to cart</Button>
       </CardContent>
     </Card>
   );
