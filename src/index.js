0df9e5c7c69c615cfe07afba99f5208a28d8eab7
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-10 20:30:49 -0700

    Added comment to show the redux aspects of the code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+// Redux stuff
 import { Provider } from 'react-redux';
 import createStore from './store';
 
