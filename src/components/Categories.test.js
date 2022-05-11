d7eb1604794f5f6a2bf0ef45f7bb8bbff765bc5f
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-10 20:31:00 -0700

    Created unit test for Categories component

---
+++ src/components/Categories.test.js
@@ -0,0 +1,44 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Categories from './Categories';
+
+import { Provider } from 'react-redux';
+import createStore from '../store';
+
+describe('Testing the Categories component', () => {
+  test('Categories component can render', () => {
+    render(
+      <Provider store={createStore()}>
+        <Categories />
+      </Provider>
+    );
+  });
+
+  it('Has a select element', () => {
+    render(
+      <Provider store={createStore()}>
+        <Categories />
+      </Provider>
+    );
+
+    let selectElem = screen.getByTestId(/categories-select/i);
+
+    expect(selectElem).toBeInTheDocument();
+  });
+
+  it('Has menu-item elements', () => {
+    render(
+      <Provider store={createStore()}>
+        <Categories />
+      </Provider>
+    );
+
+    // When testing a MUI Select component, screening for and then clicking on the Select component itself does nothing
+    // You need to find the button-role HTML element that lives inside the component
+    let selectElem = screen.getByRole('button');
+    fireEvent.mouseDown(selectElem);
+
+    let selectionElem = screen.getByTestId(/Electronics/i);
+
+    expect(selectionElem).toBeInTheDocument();
+  });
+});
