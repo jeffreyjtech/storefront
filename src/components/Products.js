9cf2b3b247597a7c0b2b6cbf9092115f19004cdd
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-10 23:29:06 -0700

    Adjusted styling so category select is just right of H2

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,11 +5,19 @@ import shortUUID from 'short-uuid';
 
 import Product from './Product';
 import { Grid, Typography } from '@mui/material';
+import Categories from './Categories';
 
 function Products({ filteredProducts, filterProducts, resetProducts }) {
   return (
     <div className="products-display">
-      <Typography variant="h2">Browse our wares</Typography>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <Typography variant="h2" sx={{ display: 'inline' }}>
+          Browse our wares
+        </Typography>
+        <span>
+          <Categories />
+        </span>
+      </div>
       <Grid container spacing={2}>
         {filteredProducts.map((product) => (
           <Grid key={shortUUID.generate()} item xs={12} sm={6} md={3} lg={2}>
