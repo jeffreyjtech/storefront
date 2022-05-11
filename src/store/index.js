3104b72d9ef718a9dd5da91f4e007f5f5bd732ad
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-10 23:28:22 -0700

    Added cartReducer to reducer combiner

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,12 @@ import { composeWithDevTools } from '@redux-devtools/extension';
 
 import productsReducer from './products';
 import categoriesReducer from './categories';
+import cartReducer from './cart';
 
 let reducers = combineReducers({
   products: productsReducer,
   categories: categoriesReducer,
+  cart: cartReducer,
 });
 
 export default function store() {
