521a1727d83abc17b22048f084b7937fea47ccc0
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-11 23:42:25 -0700

    Added thunk middleware

---
+++ src/store/middleware/thunk.js
@@ -0,0 +1,11 @@
+// A thunk is an action that occurs after something else has already
+// Return our action after it runs
+
+const thunk = (store) => (next) => (action) => {
+  if (typeof action === 'function') {
+    action(store.dispatch, store.getState);
+  }
+  next(action);
+};
+
+export default thunk;
