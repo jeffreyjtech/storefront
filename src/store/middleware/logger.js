91217524e6b97499ac793d4d47bf6e99ff29ceba
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-11 23:42:15 -0700

    Removed logger since it complicates adding the thunk middleware

--- src/store/middleware/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = (store) => (next) => (action) => {
-  console.log("**Action**:", action);
-  return next(action); // moves the action to the next middleware or the reducer if no middleware exists
-}
-
-export default logger;
