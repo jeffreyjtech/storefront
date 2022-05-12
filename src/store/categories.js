94c173d9da8fc12a38eea5153fb6e8f7e54dce6c
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-12 01:08:26 -0700

    Added GET categories and removed initial categories state

--- src/store/categories.js
+++ src/store/categories.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
+
 const allItemsCategory = {
-  key: 'all',
+  name: 'all',
   displayName: 'All',
   description: 'All categories',
 };
@@ -10,16 +12,6 @@ const initialState = {
     {
       ...allItemsCategory,
     },
-    {
-      key: 'food',
-      displayName: 'Food',
-      description: 'Human fuel',
-    },
-    {
-      key: 'electronics',
-      displayName: 'Electronics',
-      description: 'Nerd fuel',
-    },
   ],
 };
 
@@ -27,9 +19,21 @@ export default function categoriesReducer(state = initialState, { type, payload 
   const { categories } = state;
 
   switch (type) {
+    case 'GET_CATEGORIES':
+      const retrievedCategories = payload.results.reduce((accumulator, current) => {
+        if (!accumulator.includes(current)) {
+          accumulator.push(current);
+        }
+        return accumulator;
+      }, []);
+
+      return { ...state, categories: [allItemsCategory, ...retrievedCategories] };
+
     case 'ACTIVE':
       // action requires a payload object with property category
-      const selection = categories.find((category) => category.key === payload.category);
+      const selection = categories.find(
+        (category) => category.name === payload.category.name
+      );
       if (selection) {
         return { ...state, activeCategory: selection };
       }
@@ -43,7 +47,17 @@ export default function categoriesReducer(state = initialState, { type, payload 
   }
 }
 
+export const getCategories = () => async (dispatch, getState) => {
+  let response = await axios.get('https://api-js401.herokuapp.com/api/v1/categories');
+  console.log(response.data);
+  dispatch({
+    type: 'GET_CATEGORIES',
+    payload: response.data,
+  });
+};
+
 export const setActiveCategory = (category) => {
+  console.log('Setting active category', category);
   return {
     type: 'ACTIVE',
     payload: { category },
