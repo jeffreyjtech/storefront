274601c71f46093f94fc719501fc9ba2831187d3
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-12 23:04:48 -0700

    Renamed productId to _id, added details page link

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,17 @@
+import { Info } from '@mui/icons-material';
 import { Button, Card, CardContent, CardHeader } from '@mui/material';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { addToCart } from '../store/cart';
 import { removeFromStock } from '../store/products';
 
 export default function Product({ product }) {
-  const { name, price, description, category, productId, inStock } = product;
+  const { name, price, description, category, _id, inStock } = product;
 
   const dispatch = useDispatch();
 
   return (
-    <Card variant="outlined" data-testid={`product-${category}-${productId}`}>
+    <Card variant="outlined" data-testid={`product-${category}-${_id}`}>
       <CardHeader title={name} />
       <CardContent>
         <p>{description}</p>
@@ -20,9 +22,14 @@ export default function Product({ product }) {
             dispatch(addToCart(product));
             dispatch(removeFromStock(product, 1));
           }}
-          data-testid={`addtocart-${productId}`}
+          data-testid={`addtocart-${_id}`}
         >
           Add to cart
+        </Button>
+        <Button sx={{ float: 'right' }}>
+          <Link to={`/products/${_id}`} style={{color:'inherit'}}>
+            <Info />
+          </Link>
         </Button>
       </CardContent>
     </Card>
