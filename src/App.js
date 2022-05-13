396dce868e103e00479fdeb17aaf5a6881172b1e
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-12 21:57:27 -0700

    Added Router with routes home and product

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import Header from './components/Header';
-import Products from './components/Products';
 import Footer from './components/Footer';
-import { Box } from '@mui/material';
-import SimpleCart from './components/SimpleCart';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import ProductDetails from './routes/ProductDetails';
 
 function App() {
   return (
     <div className="App">
-      <Box sx={{ p: 2 }}>
-        <Header />
-        <Box sx={{ float: 'right' }}>
-          <SimpleCart />
-        </Box>
-        <Products />
-        <Footer />
-      </Box>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:productId" element={<ProductDetails />}>
+        </Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
