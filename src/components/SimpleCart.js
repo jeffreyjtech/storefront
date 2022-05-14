eb77f3a59891263b18fce5e6b5e99b5226548e7c
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-13 19:15:07 -0700

    Added remove-from-cart button and associated store logic

--- src/components/SimpleCart.js
+++ src/components/SimpleCart.js
@@ -1,17 +1,27 @@
-import { List, ListItem, ListItemText } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { RemoveCircle } from '@mui/icons-material';
+import { List, ListItem, ListItemText, Button } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
 import shortUUID from 'short-uuid';
+import { removeFromCart } from '../store/cart';
 
 export default function SimpleCart() {
   const contents = useSelector((state) => state.cart.contents);
+  const dispatch = useDispatch();
 
   return (
-    <List sx={{position: 'absolute', right:'100px'}}>
+    <List sx={{ position: 'absolute', right: '100px' }}>
       Your cart:
       {contents.map((product) => (
         <ListItem key={shortUUID.generate()}>
           <ListItemText>
-            {product.displayName} <br/> qty: {product.quantity}
+            {product.displayName} <br /> qty: {product.quantity}
+            <Button
+              onClick={() => {
+                dispatch(removeFromCart(product));
+              }}
+            >
+              <RemoveCircle />
+            </Button>
           </ListItemText>
         </ListItem>
       ))}
